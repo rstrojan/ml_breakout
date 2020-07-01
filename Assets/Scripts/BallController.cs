60425f2dd50ed926b53cd8eba9745c958ea066f4
elliochr <46430710+elliochr@users.noreply.github.com>
2020-07-01 13:42:15 -0700

    updated ball motion to reflect

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float boundaryZ = 15;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z >= boundaryZ){
+            changeLookForward(new Vector3(0, 0, -1));
+        }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        
+        ContactPoint hitAngle = other.contacts[0];       // get point of contact with other object
+        Debug.Log("hitAngle: " + hitAngle.normal);
+        changeLookForward(hitAngle.normal);
+        Debug.Log("forward: " + transform.forward);
+    }
+
+    private void changeLookForward(Vector3 barrier){
+        Vector3 newAngle = Vector3.Reflect(transform.forward.normalized, barrier.normalized); // get angle of reflection
+        transform.forward = new Vector3(newAngle.x, 0, newAngle.z); // assign foward to angle of relection
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 15.0f;
-    public float xRange = 15.0f;
+    public float speed = 20.0f;
+    private float xRange = 20.0f;
     private Vector3 startPos;
+
+    private Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
         startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Limit player motion from left to right
+        MovePlayer();
+        ConstrainPlayer();        
+    }
+
+    // get player input to move left or right
+    private void MovePlayer(){
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+    }
+
+    // Limit player motion from left to right
+    private void ConstrainPlayer(){
         if(transform.position.x < -xRange){
             transform.position = new Vector3(-xRange, startPos.y, startPos.z);
         }
         if(transform.position.x > xRange){
             transform.position = new Vector3(xRange, startPos.y, startPos.z);
         }
-
-        // get player input to move left or right
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
-        
     }
 }
