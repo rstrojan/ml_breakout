09826b923849aa316b57424019c8a247ab4bdbc9
Elliot Hershberg <eahershberg@gmail.com>
2020-07-27 09:26:35 -0700

    Add skeleton of PaddleAgent Script w/ OnEpisodeBegin

---
+++ Assets/Scripts/PaddleAgent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class PaddleAgent : Agent
+{
+    Rigidbody rBody;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // each time a new training episode begins, instantiate
+    // a new GameManager, call StartGame method
+    public override void OnEpisodeBegin()
+    {
+        GameManager manager = new GameManager();
+        manager.StartGame();
+    }
+
+    // Observations:
+    // - distance from paddle to ball
+    // - direction of ball relative to center of paddle
+    // - distance from each wall to edge of paddle
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        
+
+
+    }
+
+    // Rewards:
+    // - tiny reward for the ball moving
+    // - tiny reward for deflecting ball with paddle
+    // - large reward for breaking bricks
+    // - large penalty for losing ball
+    public override void OnActionReceived(float[] vectorAction)
+    {
+
+
+
+        
+    }
+
+}
